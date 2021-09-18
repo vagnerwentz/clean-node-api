a5065a1eca4846289c6b3c35e97bfcc2190e4cd2
Vagner Wentz <wentz.vagner@gmail.com>
2021-09-18 20:48:40 -0300

    tes: ensure SignUpController returns 500 if password confirmation fails

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -94,6 +94,25 @@ describe("SignUp Controller", () => {
     );
   });
 
+  it("Should return 400 if password confirmation fails.", () => {
+    const { systemUnderTest } = makeSystemUnderTest();
+
+    const httpRequest = {
+      body: {
+        name: "any_name",
+        email: "any_email@mail.com",
+        password: "any_password",
+        passwordConfirmation: "invalid_password",
+      },
+    };
+
+    const httpResponse = systemUnderTest.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(
+      new InvalidParamError("passwordConfirmation")
+    );
+  });
+
   it("Should return 400 if an invalid email is provided.", () => {
     const { systemUnderTest, emailValidatorStub } = makeSystemUnderTest();
 
