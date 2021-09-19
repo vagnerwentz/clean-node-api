eac7ef94b0ecc7b1bf6630d546455ee7de0553f1
Vagner Wentz <wentz.vagner@gmail.com>
2021-09-19 01:44:10 -0300

    refactor: move System Under Test creation to a factory helper method

--- src/data/useCases/addAccount/db-add-account.spec.ts
+++ src/data/useCases/addAccount/db-add-account.spec.ts
@@ -1,14 +1,28 @@
+import { Encrypter } from "../../protocols/encrypter";
 import { DbAddAccount } from "./db-add-account";
 
+interface SystemUnderTestTypes {
+  systemUnderTest: DbAddAccount;
+  encrypterStub: Encrypter;
+}
+
+const makeSystemUnderTest = (): SystemUnderTestTypes => {
+  class EncrypterStub {
+    async encrypt(value: string): Promise<string> {
+      return new Promise((resolve) => resolve("hashed_password"));
+    }
+  }
+  const encrypterStub = new EncrypterStub();
+  const systemUnderTest = new DbAddAccount(encrypterStub);
+
+  return {
+    systemUnderTest,
+    encrypterStub,
+  };
+};
 describe("DbAddAccount UseCase", () => {
   it("Should call Encrypter with correct password.", async () => {
-    class EncrypterStub {
-      async encrypt(value: string): Promise<string> {
-        return new Promise((resolve) => resolve("hashed_password"));
-      }
-    }
-    const encrypterStub = new EncrypterStub();
-    const systemUnderTest = new DbAddAccount(encrypterStub);
+    const { systemUnderTest, encrypterStub } = makeSystemUnderTest();
     const accountData = {
       name: "valid_name",
       email: "valid_email@mail.com",
