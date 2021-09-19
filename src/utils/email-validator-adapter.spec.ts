0ea85bf380ada0a8fcece65e5791bf1e39adc777
Vagner Wentz <wentz.vagner@gmail.com>
2021-09-19 01:27:06 -0300

    refactor: move System Under Test creation to a factory helper method

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -8,22 +8,26 @@ jest.mock("validator", () => ({
   },
 }));
 
+const makeSystemUnderTest = (): EmailValidatorAdapter => {
+  return new EmailValidatorAdapter();
+};
+
 describe("EmailValidator Adapter", () => {
   it("Should return false if validator returns false.", () => {
-    const systemUnderTest = new EmailValidatorAdapter();
+    const systemUnderTest = makeSystemUnderTest();
     jest.spyOn(validator, "isEmail").mockReturnValueOnce(false);
     const isValid = systemUnderTest.isValid("invalid_email@mail.com");
     expect(isValid).toBe(false);
   });
 
   it("Should return true if validator returns true.", () => {
-    const systemUnderTest = new EmailValidatorAdapter();
+    const systemUnderTest = makeSystemUnderTest();
     const isValid = systemUnderTest.isValid("invalid_email@mail.com");
     expect(isValid).toBe(true);
   });
 
   it("Should call validator with correct email.", () => {
-    const systemUnderTest = new EmailValidatorAdapter();
+    const systemUnderTest = makeSystemUnderTest();
     const isEmailSpy = jest.spyOn(validator, "isEmail");
     systemUnderTest.isValid("any_email@mail.com");
     expect(isEmailSpy).toHaveBeenCalledWith("any_email@mail.com");
