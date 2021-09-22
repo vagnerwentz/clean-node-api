a848a628d1729ea9c4818538701678cdd99a8f0e
Vagner Wentz <wentz.vagner@gmail.com>
2021-09-21 22:44:10 -0300

    test: ensure BcryptAdapter returns a hash on success

--- src/infra/criptography/bcrypt-adapter.test.ts
+++ src/infra/criptography/bcrypt-adapter.test.ts
@@ -1,6 +1,12 @@
-import bcrypt from "bcrypt";
+import bcrypt, { hash } from "bcrypt";
 
 import { BcryptAdater } from "./bcrypt-adapter";
+
+jest.mock("bcrypt", () => ({
+  async hash(): Promise<string> {
+    return new Promise((resolve) => resolve("hash"));
+  },
+}));
 
 describe("Bcrypt Adapter", () => {
   it("Should call bcrypt with correct values.", async () => {
@@ -9,5 +15,12 @@ describe("Bcrypt Adapter", () => {
     const hashSpy = jest.spyOn(bcrypt, "hash");
     await systemUnderTest.encrypt("any_value");
     expect(hashSpy).toHaveBeenCalledWith("any_value", SALT);
+  });
+
+  it("Should return a hash on success.", async () => {
+    const SALT = 12;
+    const systemUnderTest = new BcryptAdater(SALT);
+    const hash = await systemUnderTest.encrypt("any_value");
+    expect(hash).toBe("hash");
   });
 });
