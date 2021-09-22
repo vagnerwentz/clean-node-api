b0a215fcb338ef546ec26e033165b8fe0985610a
Vagner Wentz <wentz.vagner@gmail.com>
2021-09-21 22:44:03 -0300

    feat: ensure BcryptAdapter returns a hash on success

--- src/infra/criptography/bcrypt-adapter.ts
+++ src/infra/criptography/bcrypt-adapter.ts
@@ -8,7 +8,7 @@ export class BcryptAdater implements Encrypter {
     this.salt = salt;
   }
   async encrypt(value: string): Promise<string> {
-    await bcrypt.hash(value, this.salt);
-    return null;
+    const hash = await bcrypt.hash(value, this.salt);
+    return hash;
   }
 }
