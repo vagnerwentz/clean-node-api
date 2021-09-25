06b154cea354bf690d2915deae06fb9afcbc71b2
Vagner Wentz <wentz.vagner@gmail.com>
2021-09-25 16:19:41 -0300

    feat: ensure requests return json as default content type

---
+++ src/main/middlewares/content-type.ts
@@ -0,0 +1,10 @@
+import { Request, Response, NextFunction } from "express";
+
+export const contentType = (
+  request: Request,
+  response: Response,
+  next: NextFunction
+) => {
+  response.type("json");
+  next();
+};
