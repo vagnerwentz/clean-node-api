4099158bb8c50718c66b79d4a237f437fc25cab9
Vagner Wentz <wentz.vagner@gmail.com>
2021-09-25 19:34:03 -0300

    fix: fix

--- src/main/routes/signup-routes.ts
+++ src/main/routes/signup-routes.ts
@@ -1,7 +1,8 @@
 import { Router } from "express";
 
+import { adpterRoute } from "../adapters/express-route-adapter";
+import { makeSignUpController } from "../factories/signup";
+
 export default (router: Router): void => {
-  router.post("/signup", (request, response) => {
-    response.json({ ok: "ok" });
-  });
+  router.post("/signup", adpterRoute(makeSignUpController()));
 };
